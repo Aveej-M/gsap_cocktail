bca29bc91e742e0403b901e8d26a23cf668970e6
Aveej-M <aveejmk@gmail.com>
2025-06-26 17:33:21 +0530

    initial commit

---
+++ jsm_gsap_landing/src/App.jsx
@@ -0,0 +1,12 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className="text-3xl text-amber-300">App</div>
+  )
+}
+
+export default App
